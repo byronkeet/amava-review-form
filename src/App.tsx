bea5ecba5819d2fc93357d03dfd76ca6b0000f4c
Byron Keet <keetbis@gmail.com>
2025-02-14 18:09:08 +0200

    Update permission on supabase

--- src/App.tsx
+++ src/App.tsx
@@ -270,38 +270,41 @@ function App() {
   const handleSubmit = useCallback(async () => {
     setIsSubmitting(true);
     try {
-      const { error } = await supabase.from("review").insert([
-        {
-          language,
-          full_name: formState.fullName as string,
-          email: formState.email as string,
-          nationality: formState.nationality as string,
-          travel_agent: formState.travelAgent as string,
-          wildlife_experience: formState.wildlifeExperience as string,
-          guide: formState.guide as string,
-          guide_rating: formState.guideRating as string,
-          key_sightings: formState.keySightings
-            ? JSON.parse(formState.keySightings as string)
-            : [],
-          activities: formState.activities
-            ? JSON.parse(formState.activities as string)
-            : [],
-          wildlife_comments: formState.wildlifeComments as string,
-          accommodation_rating: formState.accommodationRating as string,
-          facilities_rating: formState.facilitiesRating as string,
-          food_rating: formState.foodRating as string,
-          housekeeping_rating: formState.housekeepingRating as string,
-          staff_rating: formState.staffRating as string,
-          staff_standout: formState.staffStandout
-            ? JSON.parse(formState.staffStandout as string)
-            : [],
-          hospitality_comments: formState.hospitalityComments as string,
-          recommend_tuludi: formState.recommendTuludi as string,
-          communication_rating: formState.communicationRating as string,
-          marketing_source: formState.marketingSource as string,
-          overall_experience: formState.overallExperience as string,
-        },
-      ]);
+      const { data, error } = await supabase
+        .from("review")
+        .insert([
+          {
+            language,
+            full_name: formState.fullName as string,
+            email: formState.email as string,
+            nationality: formState.nationality as string,
+            travel_agent: formState.travelAgent as string,
+            wildlife_experience: formState.wildlifeExperience as string,
+            guide: formState.guide as string,
+            guide_rating: formState.guideRating as string,
+            key_sightings: formState.keySightings
+              ? JSON.parse(formState.keySightings as string)
+              : [],
+            activities: formState.activities
+              ? JSON.parse(formState.activities as string)
+              : [],
+            wildlife_comments: formState.wildlifeComments as string,
+            accommodation_rating: formState.accommodationRating as string,
+            facilities_rating: formState.facilitiesRating as string,
+            food_rating: formState.foodRating as string,
+            housekeeping_rating: formState.housekeepingRating as string,
+            staff_rating: formState.staffRating as string,
+            staff_standout: formState.staffStandout
+              ? JSON.parse(formState.staffStandout as string)
+              : [],
+            hospitality_comments: formState.hospitalityComments as string,
+            recommend_tuludi: formState.recommendTuludi as string,
+            communication_rating: formState.communicationRating as string,
+            marketing_source: formState.marketingSource as string,
+            overall_experience: formState.overallExperience as string,
+          },
+        ])
+        .select();
 
       if (error) {
         console.error("Error submitting form:", error);
